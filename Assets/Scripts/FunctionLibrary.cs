786ebe44f7b32d22cbba7e0436188853426d2fb5
Teodes <jgrachot@gmail.com>
2023-01-28 17:12:02 -0300

    Rotating Twisting Sphere

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -56,11 +56,12 @@ public static class FunctionLibrary
 
     private static Vector3 Sphere(float u, float v, float t)
     {
-        float r = Cos(0.5f * PI * v);
+        float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+        float s = r * Cos(0.5f * PI * v);
         Vector3 p;
-        p.x = r * Sin(PI * u);
-        p.y = Sin(PI * 0.5f * v);
-        p.z = r * Cos(PI * u);
+        p.x = s * Sin(PI * u);
+        p.y = r * Sin(0.5f * PI * v);
+        p.z = s * Cos(PI * u);
         return p;
     }
 }
