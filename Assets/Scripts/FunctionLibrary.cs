c56b7ff44a6128aa4390efe7894c3966664f0a54
Teodes <jgrachot@gmail.com>
2023-01-28 17:24:50 -0300

    Twisting Torus

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -11,10 +11,11 @@ public static class FunctionLibrary
         Wave,
         MultiWave,
         Ripple,
-        Sphere
+        Sphere,
+        Torus
     }
 
-    private static readonly Function[] Functions = { Wave, MultiWave, Ripple, Sphere };
+    private static readonly Function[] Functions = { Wave, MultiWave, Ripple, Sphere, Torus };
     
     public static Function GetFunction (FunctionName name)
     {
@@ -61,6 +62,18 @@ public static class FunctionLibrary
         Vector3 p;
         p.x = s * Sin(PI * u);
         p.y = r * Sin(0.5f * PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+
+    private static Vector3 Torus(float u, float v, float t)
+    {
+        float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+        float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+        float s = r1 + r2 * Cos(PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
         p.z = s * Cos(PI * u);
         return p;
     }
