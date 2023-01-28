ea0adce705407a8ea95d51207df76a01c9cb3025
Teodes <jgrachot@gmail.com>
2023-01-27 21:38:07 -0300

    Delegate Array implemented

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -1,36 +1,35 @@
-﻿using UnityEngine;
-using static UnityEngine.Mathf;
+﻿using static UnityEngine.Mathf;
 
 public static class FunctionLibrary
 {
     // El delegates es una declaración de Tipo, lo que significa que luego debe ser instanciada
     //Se crea una "firma", que corresponde a los parámetros del delegado. Siempre deben respetarse.
     public delegate float Function (float x, float t);
+    
+    //Array del delegado llamado Functions
+    private static readonly Function[] Functions = {Wave, MultiWave, Ripple};
+    
     //Método del delegado, dicho método es estático pero eso no significa que
     //al llamarlo podamos omitir la llamada al tipo (el delegado en cuestión)
     //ya que el delegado no es estático.
-    public static Function GetFunction (int index) {
-        if (index == 0) {
-            return Wave;
-        }
-        else if (index == 1) {
-            return MultiWave;
-        }
-        else {
-            return Ripple;
-        }
+    public static Function GetFunction (int index)
+    {
+        return Functions[index];
     }
-    public static float Wave(float x, float t)
+    //Gracias a que el método GetFunction es público, ya no existe la necesidad de que
+    //los demás métodos lo sigan siendo.
+    private static float Wave(float x, float t)
     {
         return Sin(PI * (x + t));
     }
-    public static float MultiWave (float x, float t) {
+
+    private static float MultiWave (float x, float t) {
         float y = Sin(PI * (x + 0.5f * t));
         y += Sin(2f * PI * (x + t)) * 0.5f;
         return y * (2f / 3f);
     }
 
-    public static float Ripple(float x, float t)
+    private static float Ripple(float x, float t)
     {
         float d = Abs(x);
         float y = Sin(PI * (4f * d - t));
